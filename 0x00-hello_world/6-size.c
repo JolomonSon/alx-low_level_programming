f97f5f056d29e94bb3e200ce799bd1872f6430e8
John Solomon <jolomonson@gmail.com>
2022-07-05 09:49:26 -0700

    Betty coding style - trailing whitespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-
 	return (0);
 }
