b4e013ea0f2d5e89bbe4a49400377dc50b251c5f
John Solomon <jolomonson@gmail.com>
2022-07-05 09:57:55 -0700

    Betty coding style - trailing whitespace error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 	char c;
-	int a;
-	long int b;
-	long long int c;
+	int i;
+	long int a;
+	long long int b;
 	float f;
 	
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsignedlong)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsignedlong)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsignedlong)sizeof(a));
+	printf("Size of a long long int: %lu byte(s)\n", (unsignedlong)sizeof?(b));
+	printf("Size of a float: %lu byte(s)\n", (unsignedlong)sizeof(f));
 	return (0);
 }
