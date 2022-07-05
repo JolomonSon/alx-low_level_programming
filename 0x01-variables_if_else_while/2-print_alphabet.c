c71590dba9b49fc3a4811317d77da8668dafda59
John Solomon <jolomonson@gmail.com>
2022-07-05 16:08:31 -0700

    Betty coding style - code indent for conditinal statement

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
+
 /**
- * main - Entry point
  *
- * Return: Always 0 (Success)
-*/
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 
 {
@@ -14,11 +22,11 @@ int main(void)
 
 
 
-		for (low = 'a'; low <= 'z'; low++)
+			for (low = 'a'; low <= 'z'; low++)
 
 						putchar(low);
 
-					printf("\n");
+							printf("\n");
 
 
 
