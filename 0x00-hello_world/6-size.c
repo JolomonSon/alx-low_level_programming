43de716377466e1cf76f5eb81f5cfe8fbba7957e
John Solomon <jolomonson@gmail.com>
2022-07-05 10:01:10 -0700

    removed trailing whitespaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,10 @@ int main(void)
 	long int a;
 	long long int b;
 	float f;
-	
-	printf("Size of a char: %lu byte(s)\n", (unsignedlong)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsignedlong)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsignedlong)sizeof(a));
-	printf("Size of a long long int: %lu byte(s)\n", (unsignedlong)sizeof?(b));
-	printf("Size of a float: %lu byte(s)\n", (unsignedlong)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(a));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
