70b818129d5a34663364f007d0c601056aef086a
John Solomon <jolomonson@gmail.com>
2022-07-06 04:10:01 -0700

    omo i am tired with betty coding style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,17 +6,16 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
+		char alph = 'a';
 
-		char alph = 'a';		
 		while (alph <= 'z')
 		{
 			putchar(alph);
 			alph++;
 		}
 		putchar('\n');
+
 		return (0);
 }
