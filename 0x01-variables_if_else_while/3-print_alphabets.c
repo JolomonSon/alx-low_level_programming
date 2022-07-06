c998f91d7378986cb23a313e81ab9f8ade34b8b2
John Solomon <jolomonson@gmail.com>
2022-07-06 04:26:25 -0700

    executable file - 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char l_alph = 'a';
+		char u_alph = 'A';
+
+		while (l_alph <= 'z')
+		{
+			putchar(l_alph);
+			l_alph++;
+			
+		}
+		while (u_alph <= 'Z')
+		{
+			putchar(u_alph);
+			u_alph++;
+		}
+		putchar('\n');
+
+		return (0);
+}
