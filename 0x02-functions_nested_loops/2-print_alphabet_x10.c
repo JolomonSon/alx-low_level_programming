dedfbbcfbaec8232855c80c99c15794617ef1ff9
John Solomon <jolomonson@gmail.com>
2022-07-09 05:25:02 -0700

    modified 2-print_alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,14 @@ void print_alphabet_x10(void)
 	int i;
 	
 	for (i = 0; i < 10; i++)
-{
-	char alphabet_x10 = 'a';
+	{
+		char alphabet_x10 = 'a';
 
-	while (alphabet_x10 <= 'z')
-{
-	_putchar(alphabet_x10);
-	alphabet_x10++;
-}
-	_putchar('\n');
-}
+		while (alphabet_x10 <= 'z')
+	{
+		_putchar(alphabet_x10);
+		alphabet_x10++;
+	}
+		_putchar('\n');
+	}
 }
