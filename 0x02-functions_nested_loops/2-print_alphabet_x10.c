b89c44c6e8ecbb6e7ec445c012a235c8c7f60db2
John Solomon <jolomonson@gmail.com>
2022-07-09 05:15:16 -0700

    Betty coding and documentation style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - Alphabet printer.
+ * print_alphabet_x10 - X10 Alphabet printer.
  *
  * Description: Prints the alphabet in Lower Case
- * using int _putchar(int c).
+ * using int _putchar(int c) 10 times over.
  */
 void print_alphabet_x10(void)
 {
 	int i;
-	
-	for (i=0; i<10; i++)
+	for (i = 0; i < 10; i++)
 {
 	char alphabet_x10 = 'a';
-
+	
 	while (alphabet_x10 <= 'z')
 {
 	_putchar(alphabet_x10);
